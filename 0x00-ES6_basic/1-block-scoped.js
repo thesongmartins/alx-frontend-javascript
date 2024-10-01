75f5ff8c080b054e59ff59ad02af7070cb834564
thesongmartins <martinsjehoshaphat@gmail.com>
2024-10-01 03:44:21 +0100

    block scope added

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,9 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
-     let task = true;
+     const task = true;
      const task2 = false;
   }
 
