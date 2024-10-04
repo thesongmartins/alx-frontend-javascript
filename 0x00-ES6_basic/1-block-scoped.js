46318695cb7d2d7d4665863350a0557bbffc2edc
thesongmartins <martinsjehoshaphat@gmail.com>
2024-10-04 01:06:32 +0100

    task arrow update

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;  // Block-scoped, won't overwrite the outer task
-    const task2 = false; // Block-scoped, won't overwrite the outer task2
+    let task = true;  // Block-scoped, won't overwrite the outer task
+    let task2 = false; // Block-scoped, won't overwrite the outer task2
   }
 
   return [task, task2];
