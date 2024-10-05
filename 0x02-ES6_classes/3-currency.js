63370bf72858c3a3ee7d3cf2d207de597ea10185
thesongmartins <martinsjehoshaphat@gmail.com>
2024-10-05 04:10:49 +0100

    Updated

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,38 +1,30 @@
-/* Implement a class named Currency:
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
 
-- Constructor attributes:
-code (String)
-name (String)
-Each attribute must be stored in an “underscore” attribute version (ex: name is stored in _name)
-Implement a getter and setter for each attribute.
-Implement a method named displayFullCurrency that will return the attributes in the following format name (code).
-*/
-export default class currency {
-    constructor(name, code) {
-        this._name = name;
-        this._code = code;
-    }
-    //getting code
-    get code() {
-        return this._code;
-    }
-    //seting code
-    set code(newCode) {
-        this._code = newCode;
-    }
-    //get code
-    get name() {
-        return this._name;
-    }
+  get code() {
+    return this._code;
+  }
 
-    //set code
-    set name(newName) {
-        this._name = newCode;
+  set code(value) {
+    this._code = value;
+  }
 
+  get name() {
+    return this._name;
+  }
 
-    }
-    //displaying currency in the format of "Name Code"
-    displayFullCurrency() {
-        return (`${this._name} ${this._code}`)
-    }
+  set name(value) {
+    this._name = value;
+  }
+
+  /**
+   * Creates the full string representation of this Currency.
+   * @returns {String}
+   */
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
 }
